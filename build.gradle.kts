28dc64018fe096ffd1ad5fa192198e2fe4340ffe
Ight <dev@ight.io>
2025-05-17 21:19:14 -0700

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     alias(libs.plugins.vanniktech.maven.publish)
     `version-catalog`
@@ -9,3 +12,31 @@ catalog {
         from(files("${rootDir}/gradle/libs.versions.toml"))
     }
 }
+
+
+tasks.clean {
+    delete("${rootDir}/README.md")
+}
+
+val updateReadme by tasks.registering(Copy::class) {
+
+    dependsOn(tasks.clean)
+
+    val toml = projectDir.resolve("gradle/libs.versions.toml")
+    val readme = projectDir.resolve("template/README.md.template")
+    inputs.files(toml , readme)
+
+    from(readme)
+    into(layout.projectDirectory)
+
+    expand(
+        "tomlContent" to toml.readText() ,
+        "timestamp" to LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) ,
+    )
+
+    rename { "README.md" }
+}
+
+
+tasks.build { dependsOn(updateReadme) }
